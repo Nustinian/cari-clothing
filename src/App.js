e9d0269974a542f7b85bd7a5808f9601c2e3a528
Austin Atwood <austin.w.atwood@gmail.com>
2020-04-10 21:21:22 +0900

    added shop page with previews of each collection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
 
 const ItemPage = (props) => {
@@ -16,6 +17,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
         <Route path="/shop/:item" component={ItemPage} />
       </Switch>
     </div>
