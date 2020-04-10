f64195aa9a2ea494c0e2d40536a481c08b63245e
Austin Atwood <austin.w.atwood@gmail.com>
2020-04-10 16:07:37 +0900

    updated menu-items to navigate using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const ItemPage = (props) => {
+  return (
+    <div>
+      <h1>{props.match.params.item.toUpperCase()} PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/:item" component={ItemPage} />
+      </Switch>
     </div>
   );
 }
